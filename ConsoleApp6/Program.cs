89304bb7f61247adaacd105c02b6f068b50ba0b2
milad khoshkam <milad@gmail.com>
2022-05-12 14:56:32 +0430

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RedisSingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var redis = RedisRepo.SingletonInstance;
+
+            string key1 = Console.ReadLine();
+            Console.WriteLine(redis.GetMeterKey(key1));
+            string key2=  Console.ReadLine();
+            Console.WriteLine(redis.GetMeterKey(key2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp6/RedisRepo.cs
@@ -0,0 +1,47 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisSingletonPattern
+{
+    public class RedisRepo
+    {
+        #region Singleton pattern
+        private readonly static Lazy<RedisRepo> _redisRepo = new Lazy<RedisRepo>(() => new RedisRepo());
+        private readonly Lazy<ConnectionMultiplexer> _RedisConnection;
+        private IDatabase redisdb;
+        public RedisRepo()
+        {
+            _RedisConnection = new Lazy<ConnectionMultiplexer>(() => {
+
+                return ConnectionMultiplexer.Connect("localhost:6379");
+            });
+
+            redisdb = _RedisConnection.Value.GetDatabase();
+        }
+
+
+        public static RedisRepo SingletonInstance
+        {
+            get
+            {
+                return _redisRepo.Value;
+            }
+        }
+        #endregion
+
+
+
+        public string GetMeterKey(string key)
+        {
+           
+            return redisdb.StringGet(key);
+        }
+
+
+    }
+}
